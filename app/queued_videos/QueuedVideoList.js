2e7b14b73d4c4b1f6689043e2818c3e7d4fe12ae
berkcaputcu <berk.caputcu@shopify.com>
2015-09-02 03:49:41 -0400

    navigator needs work

--- app/queued_videos/QueuedVideoList.js
+++ app/queued_videos/QueuedVideoList.js
@@ -3,18 +3,61 @@ var Dispatcher = require('../../Dispatcher');
 var JukappStore = require('../stores/JukappStore');
 var JukappApi = require('../JukappApi');
 var VideoList = require('../videos/VideoList');
+var FavoriteList = require('../favorites/FavoriteList');
+var AccountDetails = require('../accounts/AccountDetails');
 
 var {
   Component,
   StyleSheet,
+  PropTypes,
   Navigator,
   View,
-  Text
+  Text,
+  TouchableHighlight
 } = React;
 
-// var routes = {
-//   inQueue:
-// }
+class TabbarItem extends Component {
+  render() {
+    var containerStyle = [styles.tabbarItemContainer];
+    var textStyle = [styles.tabbarItemText];
+
+    if (this.props.active) {
+      containerStyle.push(styles.tabbarItemContainerActive);
+      textStyle.push(styles.tabbarItemTextActive);
+    }
+
+    return (
+      <TouchableHighlight
+        style={{flex: 1}}
+        underlayColor="#F5F5F5"
+        activeOpacity={0.3}
+        onPress={this.props.onPress}
+      >
+        <View style={containerStyle}>
+          <Text style={textStyle}>{this.props.routeName.toUpperCase()}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+TabbarItem.propTypes = {
+  active: PropTypes.bool,
+  onPress: PropTypes.func,
+  routeName: PropTypes.string.isRequired
+};
+
+var routes = [
+  {
+    component: 'queue'
+  },
+  {
+    component: 'latest'
+  },
+  {
+    component: 'popular'
+  }
+];
 
 class QueuedVideoList extends Component {
 
@@ -22,7 +65,8 @@ class QueuedVideoList extends Component {
     super(props);
 
     this.state = {
-      loading: true
+      loading: true,
+      selectedTab: 0
     };
   }
 
@@ -59,33 +103,52 @@ class QueuedVideoList extends Component {
   }
 
   _renderTabBars() {
+    var tabbarItems = routes.map((route, index) => {
+      var onTabbarItemPress = () => {
+        this.setState({selectedTab: index});
+        for (var i = Math.abs(this.state.selectedTab - index); i >= 0; i--) {
+          if (index > i) {
+            this._nav.jumpForward();
+          } else if (index < i) {
+            this._nav.jumpBack();
+          }
+        }
+      };
+
+      return (
+        <TabbarItem
+          key={route.component}
+          routeName={route.component}
+          active={this.state.selectedTab == index}
+          onPress={onTabbarItemPress}
+        />
+      );
+    });
+
     return (
-      <View style={{
-        flexDirection: 'row',
-        height: 40,
-        marginTop: 20,
-        backgroundColor: 'white'
-      }}>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', borderBottomColor: '#33ADFF', borderBottomWidth: 2}}>
-          <Text style={{color: '#33ADFF', fontWeight: 'bold'}}>QUEUE</Text>
-        </View>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{color: 'black', fontWeight: 'bold'}}>LATEST</Text>
-        </View>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{color: 'black', fontWeight: 'bold'}}>POPULAR</Text>
-        </View>
+      <View style={styles.tabbarContainer}>
+        {tabbarItems}
       </View>
     );
   }
 
-  _renderScene() {
-    return (
-      <VideoList
-        videos={JukappStore.getQueuedVideos()}
-        loading={this.state.loading}
-      />
-    );
+  _renderScene(route) {
+    if (route.component == 'queue') {
+      return (
+        <VideoList
+          videos={JukappStore.getQueuedVideos()}
+          loading={this.state.loading}
+        />
+      );
+    } else if (route.component == 'latest') {
+      return (
+        <FavoriteList />
+      );
+    } else if (route.component == 'popular') {
+      return (
+        <AccountDetails />
+      );
+    }
   }
 
   render() {
@@ -93,9 +156,15 @@ class QueuedVideoList extends Component {
       <View style={{flex: 1}}>
         {this._renderTabBars()}
         <Navigator
-          initialRouteStack={['inQueue', 'latest', 'popular']}
+          ref={(component) => this._nav = component}
+          initialRoute={routes[0]}
+          initialRouteStack={routes}
           renderScene={this._renderScene.bind(this)}
           sceneStyle={styles.navigatorScene}
+          configureScene={(route, params) => {
+            console.log(route, params);
+            return Navigator.SceneConfigs.HorizontalSwipeJump;
+          }}
         />
       </View>
     );
@@ -109,7 +178,35 @@ var styles = StyleSheet.create({
     top: -20,
     right: 0,
     bottom: 0
+  },
+
+  tabbarContainer: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    backgroundColor: 'white'
+  },
+
+  // TabbarItem
+  tabbarItemText: {
+    fontWeight: 'bold'
+  },
+
+  tabbarItemTextActive: {
+    color: '#33ADFF'
+  },
+
+  tabbarItemContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  tabbarItemContainerActive: {
+    borderBottomColor: '#33ADFF',
+    borderBottomWidth: 2
   }
+
 });
 
 module.exports = QueuedVideoList;
